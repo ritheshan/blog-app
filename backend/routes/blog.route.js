49ceb7ba03468bdc18a992e0e64d61531a073b50
rithesh-n <ritheshan18@gmail.com>
2025-03-20 17:09:34 +0530

    Added blog.route.js: Set up API routes for blog functionalities.

---
+++ backend/routes/blog.route.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import {
+    createBlog,
+    getAllBlogs,
+    getBlogById,
+    updateBlog,
+    deleteBlog,
+    getUserBlogs
+} from '../controllers/blog.controller.js';
+import { authenticateUser, authorizeAdmin } from '../middleware/authUser.js';
+
+const router = express.Router();
+
+// Create a new blog
+router.post('/create', authenticateUser, authorizeAdmin, createBlog);
+
+// Delete a blog by ID
+router.delete('/delete/:id',authenticateUser,authorizeAdmin, deleteBlog);
+
+// Get all blogs
+router.get('/all-blog',authenticateUser,getAllBlogs);
+
+// Get a single blog by ID
+router.get('/blogbyid/:id', getBlogById);
+
+// get a all blog by admin
+router.get('/blogbyadmin/:id',authenticateUser,authorizeAdmin, getUserBlogs);
+
+// Update a blog by ID
+router.put('/update/:id', authenticateUser,authorizeAdmin,updateBlog);
+
+
+
+export default router;
