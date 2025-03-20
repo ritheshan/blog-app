ee7b717d2ac51f1396476f907baa9763a2a7c219
rithesh-n <ritheshan18@gmail.com>
2025-03-20 17:08:58 +0530

    Refactored user.route.js: Enhanced user route handling.

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { logoutUser, registerUser } from "../controllers/user.controller.js";
-import { loginUser } from "../controllers/user.controller.js";
+import { loginUser ,getMyProfile,getAdmins} from "../controllers/user.controller.js";
+import { authenticateUser } from "../middleware/authUser.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
 // User registration route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", logoutUser);
+router.post("/logout",authenticateUser, logoutUser);
+router.get("/myprofile",authenticateUser, getMyProfile);
+router.get("/getadmins", getAdmins);
+
 
 export default router;
