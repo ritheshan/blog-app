e210ca75cb47a602110b72caae1245e250d9d43c
rithesh-n <ritheshan18@gmail.com>
2025-03-20 17:11:03 +0530

    Modified main.jsx: Refactored app initialization for better performance.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContext } from './contacts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <AuthContext>
     <App />
+    </AuthContext>
+    
   </BrowserRouter>
 
 )
