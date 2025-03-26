018aff683f1b580e4852a8da7ee43b99881a9168
rithesh-n <ritheshan18@gmail.com>
2025-03-27 01:57:54 +0530

    chore: Improved Footer, Navbar and Home components layout

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Hero from "../Home/Hero";
+import Trending from "../Home/Trending";
+import Devotional from "../Home/Devotional";
+import Creator from "../Home/Creator";
 
-function home() {
+function Home() {
   return (
-    <div>home</div>
-  )
+    <div>
+      <Hero />
+      <Trending />
+      <Devotional />
+      <Creator />
+    </div>
+  );
 }
 
-export default home
+export default Home;
