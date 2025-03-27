86ffe773c10c6592e2c6cbe6bc327b3a072f28a7
rithesh-n <ritheshan18@gmail.com>
2025-03-27 11:55:42 +0530

    chore : update path

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Hero from "../Home/Hero";
-import Trending from "../Home/Trending";
-import Devotional from "../Home/Devotional";
-import Creator from "../Home/Creator";
+import Hero from "../home/Hero";
+import Trending from "../home/Trending";
+import Devotional from "../home/Devotional";
+import Creator from "../home/Creator";
 
 function Home() {
   return (
